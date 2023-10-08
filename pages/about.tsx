abbe77ed6c6e52ca1bc7a663b3b1fcb718b29965
alexisvalentino <alexis01valentino@gmail.com>
2023-10-08 21:34:09 +0800

    resume gdrive update

--- pages/about.tsx
+++ pages/about.tsx
@@ -136,7 +136,7 @@ export default function about() {
           <div className="about-left__links">
             <motion.a
               variants={item}
-              href="https://drive.google.com/file/d/1AKRWT-s8w9u43u-42A9TN3tvdAwEfvWV/view?usp=sharing"
+              href="https://drive.google.com/file/d/1c40uSHQaMwtuOzbUmPyadHDRnDrVxWZe/view?usp=sharing"
               onMouseEnter={() => cursorChangeHandler("hovered")}
               onMouseLeave={() => cursorChangeHandler("")}
             >
